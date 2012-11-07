ab20049d24feea3630435c2fe329a4a3e2a9d607
Eric Bézine <gitub.commit@raspberry-sharp.org>
2012-11-07 20:39:45 +0100

    Removed dependency on libbcm2835.so

--- Raspberry.System/Timers/HighResolutionTimer.cs
+++ Raspberry.System/Timers/HighResolutionTimer.cs
@@ -1,6 +1,7 @@
 #region References
 
 using System;
+using System.Linq;
 using System.Threading;
 
 #endregion
@@ -20,10 +21,15 @@ namespace Raspberry.Timers
 
         private Thread thread;
 
+        private static readonly int nanoSleepOffset = Calibrate();
+
         #endregion
 
         #region Instance Management
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HighResolutionTimer"/> class.
+        /// </summary>
         public HighResolutionTimer()
         {
             if (!Board.Current.IsRaspberryPi)
@@ -80,11 +86,38 @@ namespace Raspberry.Timers
         /// <param name="delay">The delay.</param>
         public static void Sleep(decimal delay)
         {
+            // Based on [BCM2835 C library](http://www.open.com.au/mikem/bcm2835/)
+
+            // Calling nanosleep() takes at least 100-200 us, so use it for
+            // long waits and use a busy wait on the hires timer for the rest.
+            var start = DateTime.Now.Ticks;
+
             if (delay >= 100)
-                Thread.Sleep((int)delay);
+            {
+                // Do not use high resolution timer for long interval (>= 100ms)
+                Thread.Sleep((int) delay);
+            }
+            else if (delay > 0.450m)
+            {
+                var t1 = new Interop.timespec();
+                var t2 = new Interop.timespec();
+
+                // Use nanosleep if interval is higher than 450µs
+                t1.tv_sec = (IntPtr)0;
+                t1.tv_nsec = (IntPtr)((long) (delay * 1000000) - nanoSleepOffset);
+
+                Interop.nanosleep(ref t1, ref t2);
+            }
             else
-                Interop.bcm2835_delayMicroseconds((uint)(delay * 1000));
+            {
+                while (true)
+                {
+                    if ((DateTime.Now.Ticks - start) * 0.0001m >= delay)
+                        break;
+                }
+            }
         }
+
 
         /// <summary>
         /// Starts this instance.
@@ -125,6 +158,28 @@ namespace Raspberry.Timers
         #endregion
 
         #region Private Helpers
+
+        private static int Calibrate()
+        {
+            const int referenceCount = 1000;
+            return Enumerable.Range(0, referenceCount)
+                .Aggregate(
+                    (long) 0,
+                    (a, i) =>
+                        {
+                            var t1 = new Interop.timespec();
+                            var t2 = new Interop.timespec();
+
+                            t1.tv_sec = (IntPtr) 0;
+                            t1.tv_nsec = (IntPtr) 1000000;
+
+                            var start = DateTime.Now.Ticks;
+                            Interop.nanosleep(ref t1, ref t2);
+
+                            return a + ((DateTime.Now.Ticks - start) * 100 - 1000000);
+                        },
+                    a => (int)(a / referenceCount));
+        }
 
         private void ThreadProcess()
         {
--- Raspberry.System/Timers/Interop.cs
+++ Raspberry.System/Timers/Interop.cs
@@ -1,13 +1,34 @@
+#region References
+
+using System;
 using System.Runtime.InteropServices;
+
+#endregion
 
 namespace Raspberry.Timers
 {
     internal static class Interop
     {
+        #region Constants
+
+        public static int CLOCK_MONOTONIC_RAW = 4;
+
+        #endregion
+
+        #region Classes
+
+        public struct timespec
+        {
+            public IntPtr tv_sec; /* seconds */
+            public IntPtr tv_nsec; /* nanoseconds */
+        }
+
+        #endregion
+
         #region Methods
 
-        [DllImport("libbcm2835.so", EntryPoint = "bcm2835_delayMicroseconds")]
-        public static extern void bcm2835_delayMicroseconds(uint microseconds);
+        [DllImport("libc.so.6")]
+        public static extern int nanosleep(ref timespec req, ref timespec rem); 
 
         #endregion
     }
